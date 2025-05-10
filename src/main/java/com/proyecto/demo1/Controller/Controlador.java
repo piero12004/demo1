95bc294a9f5660fdf11496b82f02431a617c83e9
piero12004 <pierobajonero.12004@gmail.com>
2025-05-10 17:19:38 -0500

    cambiar nombre html de controlador

--- src/main/java/com/proyecto/demo1/Controller/Controlador.java
+++ src/main/java/com/proyecto/demo1/Controller/Controlador.java
@@ -1,7 +1,10 @@
 package com.proyecto.demo1.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -24,6 +27,21 @@ public class Controlador {
     public String pCartelera() {
         return "cartelera";
     }
-
+    @GetMapping("/login")
+    public String pLogin() {
+        return "login"; 
+    }
+    
+    //VALIDAR USUARIO 
+    @PostMapping("/validar")
+    public String validarLogin(@RequestParam String usuario, @RequestParam String clave, Model model) {
+        if (usuario.equals("piero") && clave.equals("12004")) {
+            
+            return "adminpag";
+        } else {
+            model.addAttribute("error", "Usuario o clave incorrectos");
+            return "login";
+        }
+    }
     
 }
